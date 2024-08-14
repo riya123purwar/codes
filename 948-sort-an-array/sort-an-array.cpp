7a57a6f9031207449a51187e571d6cb67a7cdbda
riya123purwar <91371819+riya123purwar@users.noreply.github.com>
2024-08-14 20:17:51 +0530

    Time: 200 ms (47.18%) | Memory: 69.3 MB (95.60%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -3,31 +3,44 @@ public:
 
 void merge(vector<int>& a,int s, int e){
     int mid=(s+e)/2;
-    int i=s;
-    int j= mid+1;
-    int k= 0;
-    vector<int>temp;
-    while(i<= mid && j<= e){
-        if(a[i]<=a[j]){
-            temp.push_back(a[i]);
-            i++;
+    int tot_len= e-s+1;
+    int gap = (tot_len/2) + (tot_len%2);
+    while(gap>0){
+        int i=s;
+        int j = s+gap;
+        while(j<= e){
+            if(a[i]>a[j]){
+                swap(a[i],a[j]);
+            }
+            i++,j++;
         }
-        else{
-            temp.push_back(a[j]);
-            j++;
-        }
+        gap= gap<=1? 0: (gap/2)+(gap%2);
     }
-    while(i<=mid){
-       temp.push_back(a[i]); 
-       i++;
-    }
-    while(j<= e){
-        temp.push_back(a[j]);
-        j++;
-    }
-    for(k=0;k<temp.size();k++){
-        a[s+k]= temp[k];
-    }
+    // int i=s;
+    // int j= mid+1;
+    // int k= 0;
+    // vector<int>temp;
+    // while(i<= mid && j<= e){
+    //     if(a[i]<=a[j]){
+    //         temp.push_back(a[i]);
+    //         i++;
+    //     }
+    //     else{
+    //         temp.push_back(a[j]);
+    //         j++;
+    //     }
+    // }
+    // while(i<=mid){
+    //    temp.push_back(a[i]); 
+    //    i++;
+    // }
+    // while(j<= e){
+    //     temp.push_back(a[j]);
+    //     j++;
+    // }
+    // for(k=0;k<temp.size();k++){
+    //     a[s+k]= temp[k];
+    // }
 }
 
 void mergesort(vector<int>& a,int s, int e){
