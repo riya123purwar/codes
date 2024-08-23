32f7330111295297757970fca16f8aad6b5eadb2
riya123purwar <91371819+riya123purwar@users.noreply.github.com>
2024-08-23 08:58:08 +0530

    Time: 5 ms (82.00%) | Memory: 8.7 MB (71.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(),t.end());
-        if(s.compare(t)==0){
-            return true;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(),t.end());
+        // if(s.compare(t)==0){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+        int freqtable[256]={0};
+        for(int i=0;i<s.length();i++){
+            freqtable[s[i]]++;//apne aap cnvrt hojayega int me
         }
-        else{
-            return false;
+         for(int i=0;i<t.length();i++){
+            freqtable[t[i]]--;//apne aap cnvrt hojayega int me
         }
+        for(int i=0;i<256;i++){
+            if(freqtable[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
