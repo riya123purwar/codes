7050aebbcc89e5959c51e752145130935333d42e
riya123purwar <91371819+riya123purwar@users.noreply.github.com>
2024-08-25 21:23:33 +0530

    Time: 12 ms (65.66%) | Memory: 16.3 MB (63.14%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -14,10 +14,10 @@ public:
             return NULL;
         }
 
-        if(root->val == p->val){
+        if(root == p){
             return p;
         }
-        if(root->val == q->val){
+        if(root == q){
             return q;
         }
 
