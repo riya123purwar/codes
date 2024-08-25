9902aebefa9712c83258be6aff59e0989ca1151b
riya123purwar <91371819+riya123purwar@users.noreply.github.com>
2024-08-25 14:28:10 +0530

    Time: 23 ms (10.65%) | Memory: 21.5 MB (9.16%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int sum=0;int n=nums.size();
-        
-        sum = n*(1+n)/2;
-        // return n;
+        int n=nums.size();
         int s=0;
-        for(int i=0;i<n;i++){
-            // int s=0;
-             s += nums[i];
-             
+        int e= n-1;
+        int mid= s+(e-s)/2;
+        int ansin=-1;
+        sort(nums.begin(), nums.end());
+
+        while(s<=e){
+            int diff= nums[mid]- mid;
+            if(diff==0){
+                //ryt
+                s=mid+1;
+            }
+            else if(diff==1){
+                //shyd ans ho skta or nhi bhi
+                //store krwa dia
+                ansin= mid;
+                e=mid-1;
+            }
+            mid= s+(e-s)/2;
         }
-        return (sum-s);
+        if(ansin == -1){
+            return n;
+        }
+        return ansin;
+
+
     }
 };
