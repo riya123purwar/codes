62370b7690a0ccd54cb8d9211127853f56aa8b8e
riya123purwar <91371819+riya123purwar@users.noreply.github.com>
2025-09-13 02:05:21 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (9.55%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n= nums.size();
-        int s= 0;
-        int e= n-1;
-        
+        int n = nums.size();
+        int s=0;int e= n-1;
         while(s<=e){
-            int mid= (s+e)/2;
-            if(nums[mid] == target)
-            //agr hume target mill gya to hmne index return kia
-            return mid;
-            //hume right m jana h 
-            else if(target > nums[mid]){
-                 s= mid+1;
+            int mid= s+(e-s)/2;
+            if(nums[mid]== target){
+                return mid;
             }
-           //left m jana h
-            else
-            {
+            else if(target> nums[mid]){
+                s= mid+1;
+            }
+            else{
                 e= mid-1;
             }
-            //bhool jaati hu
-            // mid = (s+e)/2;
         }
-        //use kuch nhi mila mtlb use target nhi. mila
         return -1;
     }
 };
