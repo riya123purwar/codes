fca3d5a132948864fb098f250e3b18d48c52aa04
riya123purwar <91371819+riya123purwar@users.noreply.github.com>
2025-09-20 22:00:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (97.58%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& arr) {
         int n = arr.size();
+        
         int zc=0;
         int oc=0;
         int tc=0;
@@ -11,15 +12,17 @@ public:
             if(arr[i]==2) tc++;
 
         }
-        
-        for(int i=0;i<zc;i++){
-            arr[i]=0;
-        }
-        for(int i=zc;i<zc+oc;i++){
-            arr[i]=1;
-        }
-        for(int i=zc+oc;i<n;i++){
-            arr[i]=2;
-        }
+        fill(arr.begin(), arr.begin() + zc, 0);
+        fill(arr.begin()+zc, arr.begin()+zc+oc,1);
+        fill(arr.begin()+oc+zc, arr.begin()+n,2);
+        // for(int i=0;i<zc;i++){
+        //     arr[i]=0;
+        // }
+        // for(int i=zc;i<zc+oc;i++){
+        //     arr[i]=1;
+        // }
+        // for(int i=zc+oc;i<n;i++){
+        //     arr[i]=2;
+        // }
     }
 };
