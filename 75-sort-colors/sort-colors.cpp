be91e02e02ea53e2bef4f3967fcb488fd7ddbebd
riya123purwar <91371819+riya123purwar@users.noreply.github.com>
2025-09-20 22:01:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.94%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     void sortColors(vector<int>& arr) {
         int n = arr.size();
-        
-        int zc=0;
-        int oc=0;
-        int tc=0;
-        for(int i =0;i<n;i++){
-            if (arr[i]==0) zc++;
-            if(arr[i]==1) oc++;
-            if(arr[i]==2) tc++;
+        sort(arr.begin(),arr.begin()+n);
+        // int zc=0;
+        // int oc=0;
+        // int tc=0;
+        // for(int i =0;i<n;i++){
+        //     if (arr[i]==0) zc++;
+        //     if(arr[i]==1) oc++;
+        //     if(arr[i]==2) tc++;
 
-        }
-        fill(arr.begin(), arr.begin() + zc, 0);
-        fill(arr.begin()+zc, arr.begin()+zc+oc,1);
-        fill(arr.begin()+oc+zc, arr.begin()+n,2);
+        // }
+        // fill(arr.begin(), arr.begin() + zc, 0);
+        // fill(arr.begin()+zc, arr.begin()+zc+oc,1);
+        // fill(arr.begin()+oc+zc, arr.begin()+n,2);
         // for(int i=0;i<zc;i++){
         //     arr[i]=0;
         // }
